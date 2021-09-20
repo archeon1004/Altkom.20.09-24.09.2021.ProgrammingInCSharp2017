778c6b0a3bd783d024bfd7a6696b210157373b69
Paweł Łęczycki <pawel@msd.info.pl>
2021-09-20 12:41:04 +0200

    1.1

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var service = new ServiceReference.Service1Client();
+            Console.WriteLine(  service.GetData(4)  );
+
+            char key;
+            do
+            {
+                key = Console.ReadKey().KeyChar;
+            } while (key != 'f');
+        }
+    }
+}
